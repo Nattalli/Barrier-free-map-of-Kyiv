72df831d8c4dfc126758619bc52ee648da1fa257
Nataliia <dominok56@gmail.com>
2023-11-10 19:53:15 +0200

    registered models in admin panel

--- backend/pointer/admin.py
+++ backend/pointer/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import MapPoint, Addition
 
-# Register your models here.
+class AdditionAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    ordering = ['title']
+
+
+class MapPointAdmin(admin.ModelAdmin):
+    list_display = ['title', 'address', 'is_approved']
+    list_filter = ['is_approved', 'addition', 'title']
+    search_fields = ['title', 'address']
+    filter_horizontal = ('addition',)
+
+
+admin.site.register(MapPoint, MapPointAdmin)
+admin.site.register(Addition, AdditionAdmin)
