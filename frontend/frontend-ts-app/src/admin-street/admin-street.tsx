3678a65081fb2bcd94c716821739f216dfa9c0b7
Mykola <mykola.chmut@codeminers.com.ua>
2023-11-10 17:52:20 +0200

    Add devextreme

--- frontend/frontend-ts-app/src/admin-street/admin-street.tsx
+++ frontend/frontend-ts-app/src/admin-street/admin-street.tsx
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import './admin-street.scss';
 import { Outlet } from 'react-router';
-import { Link } from 'react-router-dom';
+import 'devextreme/dist/css/dx.light.css';
+import Button from 'devextreme-react/button';
+import { Link } from "react-router-dom";
 
 class AdminStreet extends Component {
 
   render() {
     return (
       <div className="admin-street">
-        <Link className="button-link" to="/admin/create">Create</Link>
+        <Link to='/admin/create'>
+          <Button type='success' text="Create"/>
+        </Link>
         <div className="admin-wrapper">
           <Outlet/>
         </div>
