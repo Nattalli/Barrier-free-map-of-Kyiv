c422bedb2d86d73256cf136c3eb9ed2c5beb167b
Dmytro Shyshko <dmytro110101@gmail.com>
2023-11-10 17:21:17 +0200

    fixed lock file

---
+++ frontend/frontend-ts-app/src/MainPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+export const MainPage = () => (
+  <div>
+    <div>Hello world! <Link to='/admin'>Your Name</Link></div>
+    main Page
+  </div>
+)
