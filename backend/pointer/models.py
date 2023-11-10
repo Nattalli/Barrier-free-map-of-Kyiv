1c23663f1902c2051df9d8c23a9a8d38f29d0399
Nataliia <dominok56@gmail.com>
2023-11-10 19:46:55 +0200

    added models and fixed the issue with ids

--- backend/pointer/models.py
+++ backend/pointer/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Addition(models.Model):
+    title = models.CharField(max_length=127)
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class MapPoint(models.Model):
+    title = models.CharField(max_length=127)
+    comment = models.TextField(blank=True, null=True)
+    address = models.CharField(max_length=255)
+    longitude = models.FloatField()
+    latitude = models.FloatField()
+    source = models.CharField(max_length=255, blank=True, null=True)
+    is_approved = models.BooleanField(default=False)
+    addition = models.ManyToManyField(Addition, blank=True)
+
+    def __str__(self) -> str:
+        return f"{self.title} at {self.address}"
