0b2569fb8d63c6b0f7ffc6b305636ebbddda34e7
Mykola <mykola.chmut@codeminers.com.ua>
2023-11-10 16:33:43 +0200

    Add application router

--- frontend/frontend-ts-app/src/App.tsx
+++ frontend/frontend-ts-app/src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import { RouterProvider, Link, createBrowserRouter } from 'react-router-dom';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Hello world! <Link to='/admin'>Your Name</Link></div>,
+    },
+    {
+      path: "/admin",
+      element: <div>Hello admin list!</div>,
+    },
+    {
+      path: "/admin/create",
+      element: <div>Hello admin create!</div>,
+    },
+    {
+      path: "/admin/:code/edit",
+      element: <div>Hello admin edit!</div>,
+    },
+    {
+      path: "/admin/:code",
+      element: <div>Hello admin edit!</div>,
+    },
+    {
+      path: "/user",
+      element: <div>Hello user list!</div>,
+    },
+    {
+      path: "/user/:code",
+      element: <div>Hello user view!</div>,
+    },
+  ]);
+
+  return (<RouterProvider router={router} />);
 }
 
 export default App;
