7f7e4a23b14a158850f693e7c1584fa007b89f9c
Nataliia <dominok56@gmail.com>
2023-11-11 11:24:54 +0200

    fixed point list endpoint

--- backend/pointer/serializers.py
+++ backend/pointer/serializers.py
@@ -1,5 +1,13 @@
 from rest_framework import serializers
-from .models import MapPoint, Addition
+from .models import MapPoint, Addition, MapPointCategory
+import math
+
+
+class SafeFloatField(serializers.FloatField):
+    def to_representation(self, value):
+        if math.isnan(value) or math.isinf(value):
+            return 0.0
+        return super().to_representation(value)
 
 
 class AdditionSerializer(serializers.ModelSerializer):
@@ -8,9 +16,18 @@ class AdditionSerializer(serializers.ModelSerializer):
         fields = ('title',)
 
 
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Addition
+        fields = ('title',)
+
+
 class MapPointSerializer(serializers.ModelSerializer):
     addition = AdditionSerializer(many=True, read_only=True)
+    category = CategorySerializer(many=False, read_only=True)
+    longitude = SafeFloatField()
+    latitude = SafeFloatField()
 
     class Meta:
         model = MapPoint
-        fields = ('title', 'comment', 'address', 'longitude', 'latitude', 'source', 'is_approved', 'addition')
+        fields = ('title', 'comment', 'address', 'longitude', 'latitude', 'source', 'is_approved', 'addition', 'category', 'schedule')
