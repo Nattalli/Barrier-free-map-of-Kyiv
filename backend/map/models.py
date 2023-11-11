370a3413b18825acd535d23f7eb862dab4a130ab
Nataliia <dominok56@gmail.com>
2023-11-11 11:48:09 +0200

    added ukrainian translate for models and their fields

--- backend/map/models.py
+++ backend/map/models.py
@@ -1,106 +1,151 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class StreetType(models.TextChoices):
-    AVENUE = 'AVENUE', 'Avenue'
-    STREET = 'STREET', 'Street'
-    BYSTREET = 'BYSTREET', 'Bystreet'
+    AVENUE = 'AVENUE', _('Проспект')
+    STREET = 'STREET', _('Вулиця')
+    BYSTREET = 'BYSTREET', _('Бічна вулиця')
 
 
 class Street(models.Model):
-    id = models.CharField(max_length=100, primary_key=True)
-    name = models.CharField(max_length=255)
-    type = models.CharField(max_length=10, choices=StreetType.choices)
-    sidewalks = models.ManyToManyField('SidewalkMap', related_name='streets')
-    central_walks = models.ManyToManyField('Sidewalk', related_name='central_streets')
-    start_with_names = models.ManyToManyField('AdjacentStreet', related_name='starting_streets')
-    end_with_names = models.ManyToManyField('AdjacentStreet', related_name='ending_streets')
+    id = models.CharField(_("ID"), max_length=100, primary_key=True)
+    name = models.CharField(_("Назва"), max_length=255)
+    type = models.CharField(_("Тип"), max_length=10, choices=StreetType.choices)
+    sidewalks = models.ManyToManyField('SidewalkMap', related_name='streets', verbose_name=_("Тротуари"))
+    central_walks = models.ManyToManyField('Sidewalk', related_name='central_streets', verbose_name=_("Центральні тротуари"))
+    start_with_names = models.ManyToManyField('AdjacentStreet', related_name='starting_streets', verbose_name=_("Початкові вулиці"))
+    end_with_names = models.ManyToManyField('AdjacentStreet', related_name='ending_streets', verbose_name=_("Кінцеві вулиці"))
+
+    class Meta:
+        verbose_name = _("Вулиця")
+        verbose_name_plural = _("Вулиці")
 
 
 class AdjacentStreet(models.Model):
-    name = models.CharField(max_length=255)
-    type = models.CharField(max_length=10, choices=StreetType.choices)
-    id = models.CharField(max_length=100, primary_key=True)
+    name = models.CharField(_("Назва"), max_length=255)
+    type = models.CharField(_("Тип"), max_length=10, choices=StreetType.choices)
+    id = models.CharField(_("ID"), max_length=100, primary_key=True)
+
+    class Meta:
+        verbose_name = _("Прилегла вулиця")
+        verbose_name_plural = _("Прилеглі вулиці")
 
 
 class Sidewalk(models.Model):
-    id = models.CharField(max_length=100, primary_key=True)
-    issues = models.ManyToManyField('SidewalkIssue', related_name='sidewalks')
-    crosswalks = models.ManyToManyField('Crosswalk', related_name='sidewalks')
-    width_in_centimeters = models.IntegerField()
-    commit_issues = models.ManyToManyField('CommitIssue', related_name='sidewalks')
+    id = models.CharField(_("ID"), max_length=100, primary_key=True)
+    issues = models.ManyToManyField('SidewalkIssue', related_name='sidewalks', verbose_name=_("Проблеми"))
+    crosswalks = models.ManyToManyField('Crosswalk', related_name='sidewalks', verbose_name=_("Пішохідні переходи"))
+    width_in_centimeters = models.IntegerField(_("Ширина в сантиметрах"))
+    commit_issues = models.ManyToManyField('CommitIssue', related_name='sidewalks', verbose_name=_("Зафіксовані проблеми"))
+
+    class Meta:
+        verbose_name = _("Тротуар")
+        verbose_name_plural = _("Тротуари")
 
 
 class SidewalkMap(models.Model):
-    left = models.ManyToManyField(Sidewalk, related_name='left_maps')
-    right = models.ManyToManyField(Sidewalk, related_name='right_maps')
+    left = models.ManyToManyField(Sidewalk, related_name='left_maps', verbose_name=_("Лівий"))
+    right = models.ManyToManyField(Sidewalk, related_name='right_maps', verbose_name=_("Правий"))
+
+    class Meta:
+        verbose_name = _("Карта тротуарів")
+        verbose_name_plural = _("Карти тротуарів")
 
 
 class IssueStatusType(models.TextChoices):
-    PROCESSED = 'PROCESSED', 'Processed'
-    INPROCESSING = 'INPROCESSING', 'Inprocessing'
-    NEW = 'NEW', 'New'
+    PROCESSED = 'PROCESSED', _('Оброблено')
+    INPROCESSING = 'INPROCESSING', _('В обробці')
+    NEW = 'NEW', _('Новий')
 
 
 class CommitIssue(models.Model):
-    username = models.CharField(max_length=63)
-    user_email = models.EmailField()
-    status = models.CharField(max_length=15, choices=IssueStatusType.choices)
-    date = models.DateTimeField()
-    issue = models.TextField()
+    username = models.CharField(_("Ім'я користувача"), max_length=63)
+    user_email = models.EmailField(_("Електронна пошта користувача"))
+    status = models.CharField(_("Статус"), max_length=15, choices=IssueStatusType.choices)
+    date = models.DateTimeField(_("Дата"), auto_now=True)
+    issue = models.TextField(_("Проблема"))
+
+    class Meta:
+        verbose_name = _("Зафіксована проблема")
+        verbose_name_plural = _("Зафіксовані проблеми")
 
 
 class SidewalkIssue(models.Model):
-    borders = models.ManyToManyField('SidewalkIssueBorder', related_name='issues')
+    borders = models.ManyToManyField('SidewalkIssueBorder', related_name='issues', verbose_name=_("Кордони"))
+
+    class Meta:
+        verbose_name = _("Проблема тротуару")
+        verbose_name_plural = _("Проблеми тротуарів")
 
 
 class SidewalkIssueBorder(models.Model):
-    height_in_centimeters = models.IntegerField()
-    GPS = models.CharField(max_length=255)
-    commit_issues = models.ManyToManyField(CommitIssue, related_name='borders')
+    height_in_centimeters = models.IntegerField(_("Висота в сантиметрах"))
+    GPS = models.CharField(_("GPS"), max_length=255)
+    commit_issues = models.ManyToManyField(CommitIssue, related_name='borders', verbose_name=_("Зафіксовані проблеми"))
+
+    class Meta:
+        verbose_name = _("Проблема кордону тротуару")
+        verbose_name_plural = _("Проблеми кордонів тротуарів")
 
 
 class CrosswalkType(models.TextChoices):
-    UNDERGROUND = 'UNDERGROUND', 'Underground'
-    OVERGROUND = 'OVERGROUND', 'Overground'
-    BY_ROAD = 'BY_ROAD', 'By Road'
+    UNDERGROUND = 'UNDERGROUND', _('Підземний')
+    OVERGROUND = 'OVERGROUND', _('Надземний')
+    BY_ROAD = 'BY_ROAD', _('Біля дороги')
 
 
 class Crosswalk(models.Model):
-    type = models.CharField(max_length=15, choices=CrosswalkType.choices)
-    issues = models.ManyToManyField('CrosswalkIssue', related_name='crosswalks')
-    benefits = models.ManyToManyField('CrosswalkBenefit', related_name='crosswalks')
-    GPS = models.CharField(max_length=255)
-    width_in_centimeters = models.IntegerField()
-    direction = models.ForeignKey('CrosswalkDirection', on_delete=models.CASCADE)
-    commit_issues = models.ManyToManyField(CommitIssue, related_name='crosswalks')
+    type = models.CharField(_("Тип"), max_length=15, choices=CrosswalkType.choices)
+    issues = models.ManyToManyField('CrosswalkIssue', related_name='crosswalks', verbose_name=_("Проблеми"))
+    benefits = models.ManyToManyField('CrosswalkBenefit', related_name='crosswalks', verbose_name=_("Переваги"))
+    GPS = models.CharField(_("GPS"), max_length=255)
+    width_in_centimeters = models.IntegerField(_("Ширина в сантиметрах"))
+    direction = models.ForeignKey('CrosswalkDirection', on_delete=models.CASCADE, verbose_name=_("Напрямок"))
+    commit_issues = models.ManyToManyField(CommitIssue, related_name='crosswalks', verbose_name=_("Зафіксовані проблеми"))
+
+    class Meta:
+        verbose_name = _("Пішохідний перехід")
+        verbose_name_plural = _("Пішохідні переходи")
 
 
 class CrosswalkIssue(models.Model):
-    border_height_in_centimeters = models.IntegerField()
-    commit_issues = models.ManyToManyField(CommitIssue, related_name='crosswalk_issues')
+    border_height_in_centimeters = models.IntegerField(_("Висота кордону в сантиметрах"))
+    commit_issues = models.ManyToManyField(CommitIssue, related_name='crosswalk_issues', verbose_name=_("Зафіксовані проблеми"))
+
+    class Meta:
+        verbose_name = _("Проблема пішохідного переходу")
+        verbose_name_plural = _("Проблеми пішохідних переходів")
 
 
 class CrosswalkBenefitType(models.TextChoices):
-    LIFT = 'LIFT', 'Lift'
-    SPECIAL_LIFT = 'SPECIAL_LIFT', 'Special Lift'
-    SOCIAL_WORKER = 'SOCIAL_WORKER', 'Social Worker'
+    LIFT = 'LIFT', _('Ліфт')
+    SPECIAL_LIFT = 'SPECIAL_LIFT', _('Спеціальний ліфт')
+    SOCIAL_WORKER = 'SOCIAL_WORKER', _('Соціальний працівник')
 
 
 class CrosswalkBenefit(models.Model):
-    type = models.CharField(max_length=15, choices=CrosswalkBenefitType.choices)
-    commit_issues = models.ManyToManyField(CommitIssue, related_name='benefits')
+    type = models.CharField(_("Тип"), max_length=15, choices=CrosswalkBenefitType.choices)
+    commit_issues = models.ManyToManyField(CommitIssue, related_name='benefits', verbose_name=_("Зафіксовані проблеми"))
+
+    class Meta:
+        verbose_name = _("Перевага пішохідного переходу")
+        verbose_name_plural = _("Переваги пішохідних переходів")
 
 
 class CrosswalkDirectionType(models.TextChoices):
-    LEFT = 'LEFT', 'Left'
-    RIGHT = 'RIGHT', 'Right'
-    TOP = 'TOP', 'Top'
-    BOTTOM = 'BOTTOM', 'Bottom'
+    LEFT = 'LEFT', _('Ліворуч')
+    RIGHT = 'RIGHT', _('Праворуч')
+    TOP = 'TOP', _('Вгору')
+    BOTTOM = 'BOTTOM', _('Вниз')
 
 
 class CrosswalkDirection(models.Model):
-    direction = models.CharField(max_length=10, choices=CrosswalkDirectionType.choices)
-    type = models.CharField(max_length=10, choices=StreetType.choices)
-    name = models.CharField(max_length=255)
-    id = models.CharField(max_length=100, primary_key=True)
+    direction = models.CharField(_("Напрямок"), max_length=10, choices=CrosswalkDirectionType.choices)
+    type = models.CharField(_("Тип"), max_length=10, choices=StreetType.choices)
+    name = models.CharField(_("Назва"), max_length=255)
+    id = models.CharField(_("ID"), max_length=100, primary_key=True)
+
+    class Meta:
+        verbose_name = _("Напрямок пішохідного переходу")
+        verbose_name_plural = _("Напрямки пішохідних переходів")
--- backend/pointer/models.py
+++ backend/pointer/models.py
@@ -1,34 +1,44 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class Addition(models.Model):
-    title = models.CharField(max_length=127)
+    title = models.CharField(_("Назва"), max_length=127)
+
+    class Meta:
+        verbose_name = _("Особливість")
+        verbose_name_plural = _("Особливості")
 
     def __str__(self) -> str:
         return self.title
 
 
 class MapPointCategory(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(_("Назва"), max_length=255)
 
     class Meta:
-        verbose_name = 'Map Point Categories'
+        verbose_name = _("Категорія точки на мапі")
+        verbose_name_plural = _("Категорії точок на мапі")
 
     def __str__(self) -> str:
         return self.title
 
 
 class MapPoint(models.Model):
-    title = models.CharField(max_length=255)
-    comment = models.TextField(blank=True, null=True)
-    address = models.CharField(max_length=255, default='Unknown')
-    longitude = models.FloatField(default=0.0)
-    latitude = models.FloatField(default=0.0)
-    source = models.CharField(max_length=255, blank=True, null=True)
-    is_approved = models.BooleanField(default=False)
-    addition = models.ManyToManyField(Addition, blank=True)
-    schedule = models.CharField(max_length=127, blank=True, null=True)
-    category = models.ForeignKey(MapPointCategory, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(_("Назва"), max_length=255)
+    comment = models.TextField(_("Коментар"), blank=True, null=True)
+    address = models.CharField(_("Адреса"), max_length=255, default=_('Невідомо'))
+    longitude = models.FloatField(_("Довгота"), default=0.0)
+    latitude = models.FloatField(_("Широта"), default=0.0)
+    source = models.CharField(_("Джерело"), max_length=255, blank=True, null=True)
+    is_approved = models.BooleanField(_("Затверджено"), default=False)
+    addition = models.ManyToManyField(Addition, verbose_name=_("Особливості"), blank=True)
+    schedule = models.CharField(_("Розклад"), max_length=127, blank=True, null=True)
+    category = models.ForeignKey(MapPointCategory, verbose_name=_("Категорія"), on_delete=models.CASCADE, null=True, blank=True)
+
+    class Meta:
+        verbose_name = _("Точка на мапі")
+        verbose_name_plural = _("Точки на мапі")
 
     def __str__(self) -> str:
-        return f"{self.title} at {self.address}"
+        return f"{self.title} за адресою {self.address}"
